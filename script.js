84f8a1651b057f19c53f2b17a2a52016f0654af8
Mte90 <mte90smanettator@gmail.com>
2012-01-18 19:24:07 +0100

    compress js

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
 $(document).ready(function() {
-	 $.backstretch("/templates/KDE4Classic/images/background.jpg", {speed: 10});
- //ACCORDION BUTTON ACTION
- $('.menu-expand').mouseover(function(e) {
-	 e.preventDefault();
-	 $(this).find('ul').stop(true, true).slideDown('normal');
- });
- $('.menu-expand').mouseout(function(e) {
-	 $(this).find('ul').stop(true, true).slideUp('normal');
- });
- $('.header-').cssAnimate({'top':'0px'},2500);
+	$.backstretch("/templates/KDE4Classic/images/background.jpg", {speed: 10});
+	$('.menu-expand').mouseover(function(e) {e.preventDefault();$(this).find('ul').stop(true, true).slideDown('normal');});
+	$('.menu-expand').mouseout(function(e) {$(this).find('ul').stop(true, true).slideUp('normal');});
+	$('.header-').cssAnimate({'top':'0px'},2500);
 
 $.getJSON("http://twitter.com/statuses/user_timeline.json?screen_name=mte90net&count=2&callback=?",
 	function(data){
